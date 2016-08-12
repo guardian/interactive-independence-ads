bd56925e50c5a45e484ad28d144f4d7402d0d4f8
Duarte Carrilho da Graça <duarte.carrilho.freelance@theguardian.com>
2016-08-12 11:36:49 +0100

    init with Labour and Liverpool series page code

---
+++ src/js/main.js
@@ -0,0 +1,90 @@
+import reqwest from 'reqwest'
+import mainHTML from './text/main.html!text'
+import share from './lib/share'
+import template from './text/template.html!text'
+import handlebars from 'handlebars'
+
+var shareFn = share('Interactive title', 'http://gu.com/p/URL', '#Interactive');
+var el;
+var data;
+
+export function init(dom, context, config, mediator) {
+    el = dom;
+    el.innerHTML = mainHTML.replace(/%assetPath%/g, config.assetPath);
+
+    var url = "https://interactive.guim.co.uk/docsdata-test/1lZsRxsU6rF4sAruy7MEIBbOlz3x3egAF9PE1wnbQPVU.json"
+    reqwest({
+        url: url,
+        type: 'json',
+        crossOrigin: true,
+        success: function(resp){
+            data = resp;
+            templateInit(config)
+        }
+    });
+}
+
+function removeHeaderContent() {
+    // Remove header content added by Labour and Liverpool template
+    var labourLiverpoolHeader = document.querySelectorAll('.content--labour-liverpool-article');
+    var labourLiverpoolStandfirst = document.querySelectorAll('.immersive-header-container--labour-liverpool .content__wrapper--standfirst');
+    var labourLiverpoolStyles = document.querySelectorAll('.immersive-header-container--labour-liverpool style');
+    var headerElements = [labourLiverpoolHeader, labourLiverpoolStandfirst, labourLiverpoolStyles];
+    var headerElement;
+
+    for (var i = 0; i < headerElements.length; i += 1) {
+        headerElement = headerElements[i];
+        if (headerElement.length) {
+            headerElement[0].remove();
+        }
+    }
+}
+
+function templateInit(config){
+    console.log(data)
+    // Header
+    var introText = data.intro_quote.split(' ');
+    var introTextLength = introText.length;
+    var newIntroText = "";
+
+    introText.forEach(function(word,i){
+        newIntroText += word + " ";
+        if(i === introTextLength - 10){
+            newIntroText += '<img src="' + config.assetPath + '/assets/imgs/Ewen.png" class="img-ewen"/>'
+        }
+    })
+
+    el.querySelector('#header-standfirst span').innerHTML = newIntroText;
+
+    removeHeaderContent();
+
+    // Rest of page
+    var mainTemplate = Handlebars.compile(template);
+    var mainTemplateParsed = mainTemplate(data).replace(/%assetPath%/g,config.assetPath);
+
+    el.querySelector('#interactive-content').innerHTML = mainTemplateParsed;
+
+    var playContainers = document.querySelectorAll('.talking-head');
+
+    for(var i =0;i <playContainers.length; i++){
+        playContainers[i].addEventListener('click',function(e){
+            var isActive = e.target.parentElement.className.indexOf('video-active') > -1 ? true : false;
+            for(var j =0;j < playContainers.length; j++){
+                playContainers[j].className = " talking-head";
+                playContainers[j].querySelector('video').pause();
+            }
+            if(!isActive){
+                e.target.parentElement.className += " video-active";
+                e.target.parentElement.querySelector('video').play();
+                e.target.parentElement.querySelector('video').autoplay = true;
+            }
+        })
+    }
+
+    var placeHolder = document.querySelector('.video-placeholder');
+
+    placeHolder.addEventListener('click',function(e){
+        el.querySelector('.interactive-video-container iframe').src = data.section_video.video_url + "#autoplay"
+        placeHolder.style.display = "none"
+    })
+}
