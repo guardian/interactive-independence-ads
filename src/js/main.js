380ab414c0dee9646ec871c7436441bbacceb10d
Duarte Carrilho da Graça <duarte.carrilho.freelance@theguardian.com>
2016-10-25 17:52:29 +0100

    base HTML & CSS for the independence page

--- src/js/main.js
+++ src/js/main.js
@@ -12,63 +12,35 @@ export function init(dom, context, config, mediator) {
     el = dom;
     el.innerHTML = mainHTML.replace(/%assetPath%/g, config.assetPath);
 
-    // var url = "https://interactive.guim.co.uk/docsdata-test/1lZsRxsU6rF4sAruy7MEIBbOlz3x3egAF9PE1wnbQPVU.json"
-    // reqwest({
-    //     url: url,
-    //     type: 'json',
-    //     crossOrigin: true,
-    //     success: function(resp){
-    //         data = resp;
-    //         templateInit(config)
-    //     }
-    // });
+
+    var content = [
+      {
+        'heading': 'The Counted',
+        'summary': 'The Counted is a project by the Guardian – and you – working to ​find the true number of people killed by police and other law enforcement agencies in the U​S​​. ​The investigation told the stories of who ​these people were, and established the trends in how they died. The US government responded​ to the revelations',
+        'link': 'https://www.theguardian.com/us-news/ng-interactive/2015/jun/01/the-counted-police-killings-us-database'
+      }, {
+        'heading': 'End FGM',
+        'summary': 'The Guardian’s Global FGM campaign has helped to get laws passed in Africa banning FGM; it persuaded the British government to write to all schools on the subject and Barack Obama to order a national survey into FGM in the US; and it has raised $4.5m from foundations to fight FGM worldwide',
+        'link': 'https://www.theguardian.com/end-fgm'
+      }
+    ]
+
+    var projectsBlock = document.querySelector('.projects-block');
+    appendContent(content, projectsBlock)
+
 }
 
-function templateInit(config){
-    // console.log(data)
-    // // Header
-    // var introText = data.intro_quote.split(' ');
-    // var introTextLength = introText.length;
-    // var newIntroText = "";
-    //
-    // introText.forEach(function(word,i){
-    //     newIntroText += word + " ";
-    //     if(i === introTextLength - 10){
-    //         newIntroText += '<img src="' + config.assetPath + '/assets/imgs/Ewen.png" class="img-ewen"/>'
-    //     }
-    // })
-    //
-    // el.querySelector('#header-standfirst span').innerHTML = newIntroText;
-    //
-    // removeHeaderContent();
-    //
-    // // Rest of page
-    // var mainTemplate = Handlebars.compile(template);
-    // var mainTemplateParsed = mainTemplate(data).replace(/%assetPath%/g,config.assetPath);
-    //
-    // el.querySelector('#interactive-content').innerHTML = mainTemplateParsed;
-    //
-    // var playContainers = document.querySelectorAll('.talking-head');
-    //
-    // for(var i =0;i <playContainers.length; i++){
-    //     playContainers[i].addEventListener('click',function(e){
-    //         var isActive = e.target.parentElement.className.indexOf('video-active') > -1 ? true : false;
-    //         for(var j =0;j < playContainers.length; j++){
-    //             playContainers[j].className = " talking-head";
-    //             playContainers[j].querySelector('video').pause();
-    //         }
-    //         if(!isActive){
-    //             e.target.parentElement.className += " video-active";
-    //             e.target.parentElement.querySelector('video').play();
-    //             e.target.parentElement.querySelector('video').autoplay = true;
-    //         }
-    //     })
-    // }
-    //
-    // var placeHolder = document.querySelector('.video-placeholder');
-    //
-    // placeHolder.addEventListener('click',function(e){
-    //     el.querySelector('.interactive-video-container iframe').src = data.section_video.video_url + "#autoplay"
-    //     placeHolder.style.display = "none"
-    // })
+function appendContent(content, block) {
+  var projectPrototype = block.querySelector('[data-please-clone-me]');
+  content.forEach(function(c) {
+    console.log('content');
+    var thisProject = projectPrototype.cloneNode(true);
+    [].forEach.call(thisProject.querySelectorAll('[data-replace-text]'), function(e) {
+      var attr = e.getAttribute('data-replace-text');
+      console.log(c, attr, c[attr]);
+      e.textContent = c[attr];
+    });
+    thisProject.removeAttribute('data-please-clone-me');
+    block.appendChild(thisProject);
+  });
 }
